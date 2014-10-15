eb4fbfe2fd9350e46d80973b6be1e4685a3c5f65
Feodor Fitsner <feodor@fitsner.com>
2014-10-14 18:26:11 -0700

    Initial check-in

---
+++ NUnitConfigTests/MyTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitConfigTests
+{
+    [TestFixture]
+    public class MyTests
+    {
+        [Test]
+        public void Test_should_access_config()
+        {
+            // appSettings
+            var someValue = ConfigurationManager.AppSettings["SomeValue"];
+            Assert.AreEqual("Hello, config!", someValue);
+
+            // connectionStrings
+            var connString = ConfigurationManager.ConnectionStrings["MyConnectionString"];
+            Assert.AreEqual("Server=(local);user=john", connString);
+        }
+    }
+}
