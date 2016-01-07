7168355c320bd1c14def46f6bf6c62936f6c603d
Jens Melgaard <admin@it-links.dk>
2016-01-07 12:49:21 +0100

    Assembly Redirect Attempt

---
+++ NUnitConfigTests/TestUsingMoq.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+
+namespace NUnitConfigTests
+{
+    [TestFixture]
+    public class TestUsingMoq
+    {
+        [Test]
+        public void TestMoqVersionRedirect()
+        {
+            var mocker = new AutoMocker();
+            mocker.Setup<IMockableThing>(mock => mock.SayHello(It.IsAny<string>())).Returns("Hello Peter");
+
+            var sut = mocker.CreateInstance<Fake>();
+
+            Assert.That(sut.SayHello("Peter"), Is.EqualTo("Hello Peter"));
+        }
+
+    }
+
+    public interface IMockableThing
+    {
+        string SayHello(string name);
+    }
+
+    public class Fake
+    {
+        private readonly IMockableThing mockable;
+
+        public Fake(IMockableThing mockable)
+        {
+            this.mockable = mockable;
+        }
+
+        public string SayHello(string name)
+        {
+            return mockable.SayHello(name);
+        }
+    }
+}
